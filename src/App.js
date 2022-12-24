38905b0073d4212e8d65aa1e79b5e4b147082226
Ryan Rogers <ryanrogers@Ryans-MacBook-Air.local>
2022-12-24 11:24:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { Component } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: "Ryan", lastName: "Rogers" },
+      company: "ZTM",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello {this.state.name.firstName} {this.state.name.lastName}, I work
+            at {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState({
+                name: { firstName: "Brittany", lastName: "Phillips" },
+              });
+            }}
+          >
+            Change Name
+          </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
