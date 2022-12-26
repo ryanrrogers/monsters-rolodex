7b0e65b433b8383a68e2b37c89bfaf218b769f75
Ryan Rogers <ryan.rogers.developer@gmail.com>
2022-12-26 07:54:39 -0500

    create CardList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 
+import CardList from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -50,13 +51,14 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <CardList />
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+    render() {
+        return (
+            <div>
+                Hello I am the cardlist component
+            </div>
+        );
+    }
+}
+
+export default CardList;
