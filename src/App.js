2918fa3cce6b920685af2315fdb4dd796293ba19
Ryan Rogers <ryan.rogers.developer@gmail.com>
2022-12-26 00:07:49 -0500

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -8,28 +7,51 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Lisa",
-        },
-        {
-          name: "Frank",
-        },
-        {
-          name: "Jacky",
-        },
-        {
-          name: "Andrei",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name
+                .toLocaleLowerCase()
+                .includes(event.target.value);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
